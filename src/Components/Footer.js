74fc2409e2fb49db37cf7cc57073aca45420fafc
Walfro-Gorilla <walfre.am@gorilla-labs.com>
2023-02-24 11:42:05 -0600

    LAST SAVED 2023
    Ultima saved del 2023 para actualizar el CV

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -23,7 +23,7 @@ class Footer extends Component {
               <ul className="social-links">{networks}</ul>
 
               <ul className="copyright">
-                <li>&copy; Copyright 2021 Wal</li>
+                <li>&copy; Copyright 2022 Wal</li>
                 <li>
                   Design by{" "}
                   <a title="Styleshout" href="http://www.gorilla-labs.com/">
